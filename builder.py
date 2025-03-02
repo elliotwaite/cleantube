d814fd73fdbee27bf51f505cbdee2c9f9922c577
elliotwaite <1767836+elliotwaite@users.noreply.github.com>
2025-03-02 12:39:47 +0800

    Fix hiding the paid content overlay

--- builder.py
+++ builder.py
@@ -1,25 +1,26 @@
 """Builds the extension."""
+
 import os
 import zipfile
 
 CUR_DIR = os.path.join(os.path.dirname(__file__))
-EXTENSION_DIR = os.path.abspath(os.path.join(CUR_DIR, 'extension'))
-BUILD_DIR = os.path.abspath(os.path.join(CUR_DIR, 'build'))
-PACKAGE_PATH = os.path.abspath(os.path.join(BUILD_DIR, 'package.zip'))
-EXCLUDED_FILENAMES = ['.DS_Store']
+EXTENSION_DIR = os.path.abspath(os.path.join(CUR_DIR, "extension"))
+BUILD_DIR = os.path.abspath(os.path.join(CUR_DIR, "build"))
+PACKAGE_PATH = os.path.abspath(os.path.join(BUILD_DIR, "package.zip"))
+EXCLUDED_FILENAMES = [".DS_Store"]
 
 
 def zip_dir(source_dir, output_path, excluded_filenames=None):
-    print(f'Zip source path: {source_dir}')
-    print(f'Zip output path: {output_path}')
-    print(f'Zipped files:')
-    with zipfile.ZipFile(output_path, 'w') as z:
+    print(f"Zip source path: {source_dir}")
+    print(f"Zip output path: {output_path}")
+    print("Zipped files:")
+    with zipfile.ZipFile(output_path, "w") as z:
         for dir_path, dir_names, filenames in os.walk(source_dir):
             for filename in filenames:
                 if filename not in excluded_filenames:
                     path = os.path.join(dir_path, filename)
                     path_in_zip = path[len(source_dir) :]
-                    print(f'  {path_in_zip}')
+                    print(f"  {path_in_zip}")
                     z.write(path, path_in_zip)
     print()
 
@@ -29,5 +30,5 @@ def main():
     zip_dir(EXTENSION_DIR, PACKAGE_PATH, EXCLUDED_FILENAMES)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
